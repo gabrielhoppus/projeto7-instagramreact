ea4e6ed462b39843b03f5c01d44cb7cdc7fab18a
gabrielhoppus <luizgabriel.84@hotmail.com>
2022-11-20 17:33:08 -0300

    project complete w/o bonus

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
+
 export default function Posts(){
     const post = [
-        {usuario: "meowed", imagem: "assets/img/meowed.svg", conteudo: "assets/img/gato-telefone.svg", curtido: "assets/img/respondeai.svg", info: "respondeai", numero: "101.523"},
-        {usuario: "barked", imagem: "assets/img/barked.svg", conteudo: "assets/img/dog.svg", curtido: "assets/img/adorable_animals.svg", info: "adorable_animals", numero: "99.159"}
-
+        {usuario: "meowed", imagem: "./assets/img/meowed.svg", conteudo: "./assets/img/gato-telefone.svg", curtido: "./assets/img/respondeai.svg", info: "respondeai", numero: 101523},
+        {usuario: "barked", imagem: "./assets/img/barked.svg", conteudo: "./assets/img/dog.svg", curtido: "./assets/img/adorable_animals.svg", info: "adorable_animals", numero: 99159}
     ]
 
     return (
@@ -22,8 +23,44 @@ export default function Posts(){
 }
 
 function Post(props){
+
+    const defaultIcon = "bookmark-outline"
+    const markedIcon = "bookmark"
+    const noLike = "heart-outline"
+    const yesLike = "heart"
+    const [icon, setIcon] = useState(defaultIcon)
+    const [like, setLike] = useState(noLike)
+    const [likes, setLikes] = useState(props.numero)
+
+    function savePost(){
+        if (icon === defaultIcon){
+            setIcon(markedIcon)
+        }else{
+            setIcon(defaultIcon)
+        }  
+    }
+
+    function likeToggle(){
+        if (like === noLike){
+            setLike(yesLike)
+            setLikes(likes+1)
+        }else{
+            setLike(noLike)
+            setLikes(likes-1)
+        }
+    }
+
+    function likePost(){
+    if (like === noLike){
+        setLike(yesLike)
+        setLikes(likes+1)
+        }else{
+            return;
+        }
+    }
+
     return (
-        <div class="post">
+        <div data-test="post" class="post">
             <div class="topo">
                 <div class="usuario">
                     <img src={props.imagem} />
@@ -34,23 +71,23 @@ function Post(props){
                 </div>
             </div>    
             <div class="conteudo">
-                <img src={props.conteudo} />
+                <img data-test="post-image" onClick={likePost} src={props.conteudo} />
             </div>    
             <div class="fundo">
                 <div class="acoes">
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon data-test="like-post" onClick={likeToggle} name={like ? like : noLike}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
+                        <ion-icon data-test="save-post" onClick={savePost} name={icon ? icon : defaultIcon}></ion-icon>
                     </div>
                 </div>    
                 <div class="curtidas">
                     <img src={props.curtido} />
                     <div class="texto">
-                        Curtido por <strong>{props.info}</strong> e <strong>outras {props.numero} pessoas</strong>
+                        Curtido por <strong>{props.info}</strong> e <strong data-test="likes-number">outras {likes.toLocaleString("pt-br")} pessoas</strong>
                     </div>
                 </div>
             </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 export default function User(){
+    const user = {
+        image: "assets/img/catanacomics.svg",
+        name: "Catana",
+        tag: "catanacomics"
+    }
+
+    const [name, setName] = useState(user.name)
+    const [image, setImage] = useState(user.image)
+
+
+    function changeName(){
+        setName(prompt("Qual é o seu novo nome?"))
+    }
+
+    function changeImage(){
+        setImage(prompt("Qual é o link para a nova imagem?"))
+    }
+
     return (
-        <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
+        <div data-test="user" class="usuario">
+            <img data-test="profile-image" onClick={changeImage} src= {image ? image : user.image} />
             <div class="texto">
-                <strong>catanacomics</strong>
-                <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                <strong>{user.tag}</strong>
+                <span data-test="name">
+                    {name ? name : user.name}
+                    <ion-icon data-test="edit-name" onClick={changeName} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
     )
 }
+
+
